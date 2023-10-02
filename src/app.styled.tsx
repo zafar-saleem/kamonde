3a5ce667a0f408904eea625b11cbfea97fdcc381
Zafar Saleem <zafarsaleem3@gmail.com>
2023-10-02 08:58:43 +0500

    style: added some padding

--- src/app.styled.tsx
+++ src/app.styled.tsx
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const Main = styled.main`
   max-width: ${props => props.theme.breakpoints.l};
   margin: auto;
+  padding-top: ${props => props.theme.tokens.gutter.gutter300};
 `;
