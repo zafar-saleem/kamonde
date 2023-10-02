2bc5d0bd3883ab926d6fa3eb472170c5576e89aa
Zafar Saleem <zafarsaleem3@gmail.com>
2023-10-02 11:55:58 +0500

    refactor: minor refactoring

--- src/components/utils/flex/Flex.styled.tsx
+++ src/components/utils/flex/Flex.styled.tsx
@@ -1,8 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { IProps } from "./interfaces";
 
-export const Flex = styled.div<IProps>`
+// @ts-ignore
+const Flex: any = css``;
+
+Flex.Flex100 = css`
+  display: flex;
+`;
+
+Flex.Flex200 = css`
+  display: flex;
+  gap: ${props => props.theme.tokens.gutter.gutter300};
+`;
+
+Flex.Flex300 = css`
+  display: flex;
+  gap: ${props => props.theme.tokens.gutter.gutter300};
+`;
+
+export const FlexWrapper = styled.div<IProps>`
   display: flex;
   flex-direction: ${props => props.direction || `row`};
   gap: ${props => `${props.gap}` || `0`};
 `;
+
+export default Flex;
--- src/components/utils/flex/Flex.tsx
+++ src/components/utils/flex/Flex.tsx
@@ -1,27 +1,9 @@
-import { css } from "styled-components";
 import { IProps } from "./interfaces";
-import * as Styled from "./Flex.styled";
-
-// @ts-ignore
-const Flex: any = css``;
-
-Flex.Flex100 = css`
-  display: flex;
-`;
-
-Flex.Flex200 = css`
-  display: flex;
-  gap: ${props => props.theme.tokens.gutter.gutter300};
-`;
-
-Flex.Flex300 = css`
-  display: flex;
-  gap: ${props => props.theme.tokens.gutter.gutter300};
-`;
+import Flex, * as Styled from "./Flex.styled";
 
 Flex.FlexWrapper = ({ children, ...props }: IProps) => {
   return (
-    <Styled.Flex {...props}>{children}</Styled.Flex>
+    <Styled.FlexWrapper {...props}>{children}</Styled.FlexWrapper>
   )
 }
 
