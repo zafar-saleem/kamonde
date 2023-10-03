f922df5ef93efd64df045a129436afa6f94699d1
Zafar Saleem <zafarsaleem3@gmail.com>
2023-10-03 11:49:04 +0500

    feat: convert item titles to links

--- src/components/pages/frameworks/components/ListItems/components/Item/Item.tsx
+++ src/components/pages/frameworks/components/ListItems/components/Item/Item.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import * as Styled from "./Item.styled";
 import Button from "./components/button";
 
@@ -11,7 +12,9 @@ export const Item = ({ ...props }) => {
         listItems?.map((item: any, index: number) => (
           <Styled.ListItem key={index}>
             <Styled.Avatar color={item.color}>{item.name.substring(0,1).toUpperCase()}</Styled.Avatar>
-            <Styled.Title>{item.name}</Styled.Title>
+            <Styled.Title>
+              <Link to="">{item.name}</Link>
+            </Styled.Title>
             <p>{trimDescription(item.description)}</p>
             <Styled.Actions>
               <Button type="likes" likes={item.likes}>❤️</Button> <Button variant="span" type="downloads" views={item.downloads}>⬇️</Button>
