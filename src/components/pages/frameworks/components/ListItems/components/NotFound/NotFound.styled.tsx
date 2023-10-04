43934fe1bc044bec6318a7d6cacffb5bc953bb89
Zafar Saleem <zafarsaleem3@gmail.com>
2023-10-04 12:53:55 +0500

    feat: improved not found result message

---
+++ src/components/pages/frameworks/components/ListItems/components/NotFound/NotFound.styled.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ContentsNotFound = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60vh;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SearchIcon = styled.span`
+  font-size: 2rem;
+`;
+
+export const NotAllowedIcon = styled.span`
+  font-size: 3rem;
+  position: absolute;
+  margin-bottom: 1rem;
+  top: -10px;
+`;
--- src/components/pages/frameworks/components/ListItems/components/NotFound/NotFound.tsx
+++ src/components/pages/frameworks/components/ListItems/components/NotFound/NotFound.tsx
@@ -1,3 +1,15 @@
+import * as Styled from "./NotFound.styled";
+
 export const NotFound = () => (
-  <p>Not Found</p>
+  <>
+  <div></div>
+  <Styled.ContentsNotFound>
+    <Styled.Container>
+      <Styled.SearchIcon>🔎</Styled.SearchIcon>
+      <Styled.NotAllowedIcon>🚫</Styled.NotAllowedIcon>
+      <span>Not Found</span>
+    </Styled.Container>
+  </Styled.ContentsNotFound>
+  <div></div>
+  </>
 )
